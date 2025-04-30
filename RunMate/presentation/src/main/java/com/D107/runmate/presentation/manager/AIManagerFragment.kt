ba8f7b1910629ebe10601906f771605ea552ac60
aoreo <hay6586@gmail.com>
2025-04-30 10:51:46 +0900

    feat: 햄버거 네비게이션 세팅

---
+++ RunMate/presentation/src/main/java/com/D107/runmate/presentation/manager/AIManagerFragment.kt
@@ -0,0 +1,17 @@
+package com.D107.runmate.presentation.manager
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.D107.runmate.presentation.R
+import com.D107.runmate.presentation.databinding.FragmentAIManagerBinding
+import com.ssafy.locket.presentation.base.BaseFragment
+
+class AIManagerFragment : BaseFragment<FragmentAIManagerBinding>(
+    FragmentAIManagerBinding::bind,
+    R.layout.fragment_a_i_manager
+) {
+
+}
