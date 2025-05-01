86be8b227ff1f03c172f1d555d5d05948f4d9207
Ubuntu <ubuntu@ip-172-26-15-93.ap-northeast-2.compute.internal>
2025-05-01 03:02:53 +0000

    Initial import: backend code from EC2

---
+++ src/main/java/com/runhwani/runmate/RunmateApplication.java
@@ -0,0 +1,21 @@
+package com.runhwani.runmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class RunmateApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RunmateApplication.class, args);
+    }
+
+    @RestController
+    static class HelloController {
+        @GetMapping("/api/hello")
+        public String hello() {
+            return "Hello, RunHwani Team!";
+        }
+    }
+}
