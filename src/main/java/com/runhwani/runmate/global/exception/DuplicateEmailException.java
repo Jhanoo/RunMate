0af5f452e46863043815715b8cdb5b30ca383c5f
김환 <kimh0414@gmail.com>
2025-05-03 09:09:56 +0900

    Revert "Merge branch 'develop/back' into 'master'"
    This reverts commit cda6dbbac3a7e880a83d15926c4e3f042425a528, reversing
    changes made to 468c919d03dd84d2dde812fa25acf4593c61522d.

--- src/main/java/com/runhwani/runmate/global/exception/DuplicateEmailException.java
+++
@@ -1,7 +0,0 @@
-package com.runhwani.runmate.global.exception;
-
-public class DuplicateEmailException extends RuntimeException {
-    public DuplicateEmailException(String message) {
-        super(message);
-    }
-} 
