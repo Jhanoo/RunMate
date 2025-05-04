043563b7ec2fb0226887e1f7a7be98dbba960147
이한나 <arinamin0123@naver.com>
2025-05-04 19:11:09 +0900

    feat:course create API S12P31D107-110

--- src/main/java/com/runhwani/runmate/config/SwaggerStartupLogger.java
+++ src/main/java/com/runhwani/runmate/config/SwaggerStartupLogger.java
@@ -12,18 +12,10 @@ public class SwaggerStartupLogger implements ApplicationListener<WebServerInitia
 
     private int port;
 
-    /**
-     * application.yml 또는 properties 에서 swagger-ui.path 를 다르게 설정하셨다면
-     * 아래 값을 그에 맞춰 변경해주세요.
-     * (기본값: /swagger-ui/index.html)
-     */
-    @Value("${springdoc.swagger-ui.path:/swagger-ui/index.html}")
-    private String swaggerPath;
-
     @Override
     public void onApplicationEvent(WebServerInitializedEvent event) {
         this.port = event.getWebServer().getPort();
-        String url = String.format("http://localhost:%d%s", port, swaggerPath);
+        String url = String.format("http://localhost:%d/swagger-ui/index.html", port);
         log.info("▶︎ Swagger UI available at: {}", url);
     }
 }
