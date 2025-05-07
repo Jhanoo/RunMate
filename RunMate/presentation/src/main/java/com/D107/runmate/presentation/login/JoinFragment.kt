5da32817c54eb74064653e328eb14166c12a4280
김미경 <rmfosem613@gmail.com>
2025-05-08 07:46:58 +0900

    feat: 로그인, 회원가입 UI 구현
    S12P31D107-153

---
+++ RunMate/presentation/src/main/java/com/D107/runmate/presentation/login/JoinFragment.kt
@@ -0,0 +1,20 @@
+package com.D107.runmate.presentation.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.D107.runmate.presentation.R
+
+class JoinFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_join, container, false)
+    }
+
+}
