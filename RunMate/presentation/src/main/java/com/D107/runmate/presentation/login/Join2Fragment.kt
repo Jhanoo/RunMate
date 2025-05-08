8dd3c6181aa88cd84454731b3bb2c325d59c5613
김미경 <rmfosem613@gmail.com>
2025-05-08 15:32:40 +0900

    feat: 화면 constraintlayout으로 변경
    S12P31D107-153

---
+++ RunMate/presentation/src/main/java/com/D107/runmate/presentation/login/Join2Fragment.kt
@@ -0,0 +1,19 @@
+package com.D107.runmate.presentation.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.D107.runmate.presentation.R
+
+class Join2Fragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_join2, container, false)
+    }
+}
