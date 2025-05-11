822518e611c84e2442620c83ddcb3267e4b570ec
김미경 <rmfosem613@gmail.com>
2025-05-12 00:20:05 +0900

    refactor: 의존성 제거

--- RunMate/domain/build.gradle.kts
+++ RunMate/domain/build.gradle.kts
@@ -17,7 +17,4 @@ dependencies {
     runtimeOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
-
-    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
-    implementation("com.squareup.okhttp3:okhttp-urlconnection:4.10.0")
 }
