72019f703a003261b2533dd0c86793e0f729eb2c
정찬우 <jhanoo15@gmail.com>
2025-05-14 13:32:53 +0900

    feat: 그룹 조회 수정 및 서버 HTTP 요청 로깅 기능 추가 S12P31D107-189

--- backend/spring-boot/src/main/java/com/runhwani/runmate/config/WebConfig.java
+++ backend/spring-boot/src/main/java/com/runhwani/runmate/config/WebConfig.java
@@ -1,8 +1,10 @@
 package com.runhwani.runmate.config;
 
+import com.runhwani.runmate.interceptor.LoggingInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -31,4 +33,12 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowCredentials(false);
     }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoggingInterceptor())
+                .addPathPatterns("/api/**")  // 모든 경로에 대해 인터셉터 적용
+                .excludePathPatterns("/css/**", "/js/**", "/images/**"); // 정적 리소스는 제외
+    }
+
 } 
