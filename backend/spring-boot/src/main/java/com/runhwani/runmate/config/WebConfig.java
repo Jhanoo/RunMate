5b85aac55510f982758a980cae9878b00ebf906e
정찬우 <jhanoo15@gmail.com>
2025-05-15 17:34:46 +0900

    fix: gpx파일 접근 권한 요청 해제

--- backend/spring-boot/src/main/java/com/runhwani/runmate/config/WebConfig.java
+++ backend/spring-boot/src/main/java/com/runhwani/runmate/config/WebConfig.java
@@ -20,9 +20,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         Path uploadPath = Paths.get(uploadDir).toAbsolutePath().normalize();
+        Path gpxPath = Paths.get("./gpx").toAbsolutePath().normalize();
         
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:" + uploadPath.toString() + "/");
+        registry.addResourceHandler("/gpx/**")
+                .addResourceLocations("file:" + gpxPath.toString() + "/");
     }
 
     @Override
